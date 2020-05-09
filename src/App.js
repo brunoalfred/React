cd974bbd506a9e1f9fd56471ea7f6391fe052286
Bruno Alfred <alfredybruno@gmail.com>
2020-05-09 23:50:26 +0300

    rendering

--- src/App.js
+++ src/App.js
@@ -1,41 +1,20 @@
 import React from 'react';
 import {render} from 'react-dom';
-
-const Pet = props => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed)
-    ]);
-}
-
-
+import Pet from './Pet';
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet, {
-            name: "Luna",
-            animal: "Dog",
-            breed: "Havanese"
-        }),
-        React.createElement(Pet, {
-            name: "Pepper",
-            animal: "Bird",
-            breed: "Cocktail"
-        }),
-        React.createElement(Pet, {
-            name: "Doink",
-            animal: "Cat",
-            breed: "Mix"
-        })
-        ]
-    );
+    
+
+    return (
+       <div>
+           <h1 id="something-important">Adopt-Me!</h1>
+           <Pet name="Luna" animal="Dog" breed="Havenese"/>
+           
+       </div>
+    )
 };
 
 render(
-    React.createElement(App),
+    <App />,
     document.getElementById("root")
 );
