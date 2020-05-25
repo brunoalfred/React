c113bcd55573a7e26f078c8e73ebdaff3f4676ec
geek <alfredybruno@gmail.com>
2020-05-25 12:02:42 +0300

    feat: Added a dropdown to with options.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,25 +1,43 @@
 // eslint-disable-next-line no-unused-vars
-import React, {useState} from "react"
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
-    
-    // eslint-disable-next-line no-undef
-    const [location, updateLocation] = useState("Seattle, WA");
+  // eslint-disable-next-line no-undef
+  const [location, updateLocation] = useState("Seattle, WA");
+  const [animal, updateAnimal] = useState("");
 
-    return(
-        <div className="search-params">
-            <form>
-                <label htmlFor="location">
-                    {location}
-                    <input id="location" 
-                    value={location} 
-                    placeholder="Location"
-                    onChange={event => updateLocation(event.target.value)}
-                    />
-                </label>
-            </form>
-        </div>
-    );
-}
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          {location}
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(event) => updateLocation(event.target.value)}
+          />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(event) => updateAnimal(event.target.value)}
+            onBlur={(event) => updateAnimal(event.target.value)}
+          >
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+      </form>
+    </div>
+  );
+};
 
 export default SearchParams;
